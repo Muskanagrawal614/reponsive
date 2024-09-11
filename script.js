f515437ff2190972129113ed580fc36f2ada695e
Muskan Agrawal <muskanagrawal614@gmail.com>
2024-09-11 13:41:19 +0545

    firstcommit

---
+++ script.js
@@ -0,0 +1,8 @@
+function ShowNav(){
+    const button = document.querySelector('.checkbtn');
+    const list=document.querySelector('ul');
+   
+    list.classList.toggle('active');
+        
+    
+}
